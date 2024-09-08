f5a88ee069fda376013426e58519e855a97aefc4
JavoxirRuziev <dolinapro065@gmail.com>
2024-09-09 00:20:48 +0500

    обновление js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 // Функция для сохранения выбранного языка в localStorage
 function saveLanguageChoice(lang) {
-    localStorage.setItem('preferredLanguage', lang);
+    // Проверяем, доступно ли localStorage
+    try {
+        localStorage.setItem('preferredLanguage', lang);
+    } catch (e) {
+        console.error('LocalStorage не доступен: ', e);
+    }
 }
 
 // Функция для проверки сохранённого языка и автоматической переадресации при необходимости
 function checkAndRedirectBasedOnLanguage() {
-    const savedLanguage = localStorage.getItem('preferredLanguage');
+    let savedLanguage;
+    try {
+        savedLanguage = localStorage.getItem('preferredLanguage');
+    } catch (e) {
+        console.error('LocalStorage не доступен: ', e);
+    }
+
     const currentLanguage = getCurrentLanguage();
 
     if (savedLanguage && savedLanguage !== currentLanguage) {
-        window.location.href = savedLanguage === 'ru' ? 'index_ru.html' : 'index_uz.html';
+        const pageToRedirect = savedLanguage === 'ru' ? 'index.html' : 'index_uz.html';
+        window.location.href = pageToRedirect;
     }
 }
 
 // Функция для определения текущего языка на основе URL
 function getCurrentLanguage() {
-    return window.location.pathname.includes('_ru.html') ? 'ru' : 'uz';
+    // Предполагаем, что URL узбекской версии содержит '_uz'
+    return window.location.pathname.includes('_uz.html') ? 'uz' : 'ru';
 }
 
-// Вызов функции проверки и переадресации при загрузке страницы
-window.onload = checkAndRedirectBasedOnLanguage;
+// Добавляем слушателя события когда контент DOM полностью загружен
+document.addEventListener('DOMContentLoaded', function() {
+    checkAndRedirectBasedOnLanguage();
+});
